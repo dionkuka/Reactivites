72f420a985bb2ffbec0daa44dbf28e3f7ced88a0
dionkuka <dion.kuka@ubt-uni.net>
2022-01-12 13:25:23 +0100

    End of section 10

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,29 +5,39 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 import { observer } from 'mobx-react-lite';
 import HomePage from '../../features/home/HomePage';
 import ActivityFrom from '../../features/activities/form/ActivityFrom';
-import { Route, useLocation } from 'react-router-dom';
+import { Route, useLocation, Switch } from 'react-router-dom';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
+import ServerError from '../../features/errors/ServerError';
 
 function App() {
   const location = useLocation();
 
   return (
     <>
+      <ToastContainer position='bottom-right' hideProgressBar />
       <Route exact path="/" component={HomePage} />
-      <Route 
+      <Route
         path={'/(.+)'}
         render={() => (
           <>
             <NavBar />
-            <Container style={{marginTop: "7em"}}>
-              <Route exact path="/activities" component={ActivityDashboard} />
-              <Route path="/activities/:id" component={ActivityDetails} />
-              <Route key={location.key} path={["/createActivity","/manage/:id"]} component={ActivityFrom} />
+            <Container style={{ marginTop: "7em" }}>
+              <Switch>
+                <Route exact path="/activities" component={ActivityDashboard} />
+                <Route path="/activities/:id" component={ActivityDetails} />
+                <Route key={location.key} path={["/createActivity", "/manage/:id"]} component={ActivityFrom} />
+                <Route path='/errors' component={TestErrors} />
+                <Route path='/server-error' component={ServerError} />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </>
         )}
       />
-      
+
     </>
   );
 }
